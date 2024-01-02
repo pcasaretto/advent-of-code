af597a5e8e3c7f4cef01cd145cfdfd0818f84067
pcasaretto <pcasaretto@gmail.com>
2024-01-02 20:58:39 -0300

    add rust tooling

--- flake.nix
+++ flake.nix
@@ -8,6 +8,16 @@
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = nixpkgs.legacyPackages.${system};
       in {
-        devShell = pkgs.mkShell { buildInputs = [ pkgs.go pkgs.gopls pkgs.gotools pkgs.go-tools ]; };
+        devShell = pkgs.mkShell { buildInputs = [
+          pkgs.go
+          pkgs.gopls
+          pkgs.gotools
+          pkgs.go-tools
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.cargo-watch
+          pkgs.rust-analyzer
+          pkgs.rustPlatform.rustcSrc
+        ]; };
       });
 }
