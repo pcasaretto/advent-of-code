33db6d9bf5343b6c71537cbb0e28a9520f77187a
pcasaretto <pcasaretto@gmail.com>
2024-01-03 15:19:32 -0300

    upgrade rust tooling

--- flake.nix
+++ flake.nix
@@ -2,11 +2,25 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.11";
     flake-utils.url = "github:numtide/flake-utils";
+    fenix.url = "github:nix-community/fenix";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = nixpkgs.legacyPackages.${system};
+      let
+      pkgs = import nixpkgs {
+        inherit system; 
+        overlays = [fenix.overlays.default]; 
+      };
+      rustToolchain = let 
+          inherit (pkgs.fenix) combine complete targets;
+        in combine [
+          complete.cargo
+          complete.rustc
+          complete.rustfmt
+          complete.clippy
+          complete.rust-analyzer
+        ];
       in {
         devShell = pkgs.mkShell {
           buildInputs = [
@@ -17,11 +31,7 @@
           pkgs.gopls
           pkgs.gotools
           pkgs.go-tools
-          pkgs.rustc
-          pkgs.cargo
-          pkgs.cargo-watch
-          pkgs.rust-analyzer
-          pkgs.rustPlatform.rustcSrc
+          rustToolchain
         ]; };
       });
 }
